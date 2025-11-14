0acb9057762a5f95d21ed19ae1267626b4b4b9c0
JulienBou07 <julien.boudreault07@hotmail.com>
2025-11-14 14:05:27 -0500

    greg

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,4 +1,6 @@
 import Icons from './utils/Icons.js';
+import Swiper from 'swiper/bundle';
+
 
 class Main {
   constructor() {
@@ -7,6 +9,19 @@ class Main {
 
   init() {
     Icons.load();
+
+    this.initSwiperPagination();
+  }
+
+    initSwiperPagination() {
+    const target = document.querySelector('.js-reactif');
+    if (target) {
+      const swiper = new Swiper(target, {
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      });
+    }
   }
 }
 new Main();
